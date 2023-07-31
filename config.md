1235f655b0d1d705e422959e95017ac0676c5544
sayed-maheen <sayedmaheen28@gmail.com>
2023-07-31 13:48:37 +0400

    commit dist only

--- config.md
+++
@@ -1,5 +0,0 @@
-.git is same as server
-
-files same as server
-only need to develop and push
-use bootstrap
--- public/index.php
+++ public/index.php
@@ -1,55 +1,24 @@
-<?php
-
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/assets/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="Full Stack Developer, Youtuber, Content Creator, Graphics Designer, Software Engineer"
+    />
+    <meta
+      name="keywords"
+      content="sayed maheen, full stack developer, portfolio website, hire me, react, threejs, django, tailwindcss, vite"
+    />
+    <meta name="author" content="Sayed Maheen" />
+    <title>Sayed Maheen | Portfolio Website - Zyed Online</title>
+    <script type="module" crossorigin src="/assets/index-8bc782c7.js"></script>
+    <link rel="stylesheet" href="/assets/index-dee071b3.css">
+  </head>
+  <body>
+    <div id="root"></div>
+    
+  </body>
+</html>
